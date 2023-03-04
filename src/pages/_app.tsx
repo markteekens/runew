8e84245e62a689ee48e59673f436dcf319be1a44
ZackCgl <mark@rubyfinance.nl>
2023-03-04 13:25:43 +0100

    merged html files

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,9 @@
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
-
 import { api } from "~/utils/api";
+
+//css imports
 import "../styles/bootstrap.min.css";
 import "../styles/color.css";
 import "../styles/fontawesome.min.css";
@@ -10,12 +11,15 @@ import "../styles/jquery.fancybox.min.css";
 import "../styles/responsive.css";
 import "../styles/style.css";
 import "../styles/swiper.css";
+
+//hier hoef je niks mee te doen, is de root file voor react, alleen css files importeren als je die nog hebt
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
     <SessionProvider session={session}>
+      {/* hierin zitten de components */}
       <Component {...pageProps} />
     </SessionProvider>
   );
